09b0e7014cb7f3ef9521eca06c1aeef856ee89b9
AniHo <anihox028@ihmeducation.se>
2024-09-24 23:04:07 +0200

    a1

---
+++ ROCK-PAPER-SCISSORS/js/app.js
@@ -0,0 +1,8 @@
+const button0= document.getElementById("button0");
+const button1 = document.getElementById("button1");
+const button2 = document.getElementById("button2");
+
+button.addEventListener("click", function() {
+
+
+});
