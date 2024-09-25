ce1a5f16fb18edf21d01d516abbdc70f43ab3a3d
AniHo <anihox028@ihmeducation.se>
2024-09-25 13:44:15 +0200

    Testing

--- ROCK-PAPER-SCISSORS/js/app.js
+++ ROCK-PAPER-SCISSORS/js/app.js
@@ -1,8 +1,17 @@
 const button0= document.getElementById("button0");
 const button1 = document.getElementById("button1");
 const button2 = document.getElementById("button2");
+const playerChoice = document.getElementById("playerChoice");
+const onclick = document.getElementById("onclick");
+const aiChoice = document.getElementById("aiChoice");;
 
-button.addEventListener("click", function() {
 
-
+onclick.addEventListener("click", () => {
+  
 });
+
+
+function getRandomButtonForPlayer() {
+  playerChoice.innerText = Math.floor(Math.random() * playerChoice.innerText);
+}
+
