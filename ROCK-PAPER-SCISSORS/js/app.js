822379ef29c45bbd2926562d35d35b9fac2bebc3
AniHo <anihox028@ihmeducation.se>
2024-09-26 11:08:02 +0200

    cookies

--- ROCK-PAPER-SCISSORS/js/app.js
+++ ROCK-PAPER-SCISSORS/js/app.js
@@ -3,15 +3,16 @@ const button1 = document.getElementById("button1");
 const button2 = document.getElementById("button2");
 const playerChoice = document.getElementById("playerChoice");
 const onclick = document.getElementById("onclick");
-const aiChoice = document.getElementById("aiChoice");;
+const aiChoice = document.getElementById("aiChoice");
 
+button0.addEventListener("click", function() {
 
-onclick.addEventListener("click", () => {
-  
 });
 
+button1.addEventListener("click", function() {
 
-function getRandomButtonForPlayer() {
-  playerChoice.innerText = Math.floor(Math.random() * playerChoice.innerText);
-}
+});
 
+button2.addEventListener("click", function() {
+
+});
--- ROLL-THE-DICE/js/app.js
+++ ROLL-THE-DICE/js/app.js
@@ -9,8 +9,15 @@ let playerRoll = 1;
 let playerAi = 2;
 let playerScore =2;
 let aiScore =3;
-// PROCESSES
 
+function tryLoadGame() {
+  playerScore = getCookie ("playerScore");
+  aiScore = getCookie ("aiScore");
+}
+
+// PROCESSES
+tryLoadGame();
+scoreResult();
 
 singleButton.addEventListener("click", function() {
 
@@ -20,6 +27,8 @@ singleButton.addEventListener("click", function() {
 showAiRollResult () ;
 evaluateResult();
 scoreResult();
+saveGame();
+
 });
 
 // CONTROLLERS
@@ -57,5 +66,21 @@ function showAiRollResult() {
 function scoreResult() {
 playerScoreText.innerText = "Player score" + playerScore;
 aiScoreText.innerText = "Ai score:" + aiScore;
+}
 
+function saveGame() {
+  document.cookie = "playerScore=" + playerScore + "; expires=Thu, 29 Dec 2024  12:00:00 UTC";
+  document.cookie = "aiScore=" + aiScore +"; expires=Thu, 29 Dec 2024  12:00:00 UTC";
+}
+
+function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i <ca.length; i++) {let c = ca[i];
+    while (c.charAt(0) === ' ') {c = c.substring(1);       }
+    if (c.indexOf(name) === 0) {return c.substring(name.length, c.length);
+    }
+  }
+  return 0;
 }
